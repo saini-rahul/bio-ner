50cd5c10b348f97bf358f7387edf575882e50a50
rssaini <rssaini@tamu.edu>
2019-05-01 13:38:15 -0500

    exit in predict

--- scripts/predict.py
+++ scripts/predict.py
@@ -8,8 +8,10 @@ model = load_saved_model()
 
 while (1):
     try:
-        input_sen = input("Type a sentence to predict slots on!\n")
+        input_sen = input("Type a sentence to predict slots on! Or enter exit to quit.\n")
     except:
         input_sen = None
+    if input_sen.lower() == 'exit':
+        break
     if input_sen:
         predict(input_sen, model)  
